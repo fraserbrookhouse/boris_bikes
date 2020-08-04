c8fd2cd1eeb990a3f09965b774046ab9db354963
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-04 15:48:25 +0100

    removed not needed fluff from spec file

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,8 +1,3 @@
 describe DockingStation do
-  describe   ".bike_release" do
-    context "person requests bike rental" do
-        it "returns release" do
-            expect(DockingStation.bike_release(true)). to eq("release") 
-        end
-    end
+
 end
--- spec/spec_DockingStation.rb
+++
@@ -1,9 +0,0 @@
-describe DockingStation do
-    describe   ".bike_release" do
-        context "person requests bike rental" do
-            it "returns release" do
-                expect(DockingStation.bike_release(true)). to eq("release") 
-            end
-        end
-    end
-end
