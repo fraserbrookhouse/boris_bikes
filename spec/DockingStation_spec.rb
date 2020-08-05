aefa8685dc2d04583be28948330f9ddc33da4349
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-05 14:41:19 +0100

    added test to check for error message when dock is full but bike tries to dock

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -23,6 +23,16 @@ describe DockingStation do
     expect(bike).to be_working
 	end
 	
+	describe '#dock' do
+		it 'raises an error when the dock is full and you try to dock a bike' do
+			bike = Bike.new
+			bike2 = Bike.new
+			subject.dock(bike)
+			expect { subject.dock(bike2) }.to raise_error 'Dock is full'
+		end
+	end
+
+
 	it { is_expected.to respond_to(:dock).with(1).argument }
 
 	it { is_expected.to respond_to(:bike) }
