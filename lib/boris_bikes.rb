2f190b5ef4155f3df064c0c119e64f8481870584
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-05 08:32:06 +0100

    added test to check if docking station can remember docked bikes, added instance varibale to dock method to save docked bike, added attribute reader for bike

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,16 +1,15 @@
+# an object that can take in, and give out bikes to people.
 class DockingStation
-
+	#releases bikes
 	def release_bike
 		Bike.new
 	end
-
+	#takes in a bike and docks it
 	def dock(bike)
 		@bike = bike
 	end
-
-	def bike
-	
-	end
+	#gives out an already docked bike
+	attr_reader :bike
 
 end
 
